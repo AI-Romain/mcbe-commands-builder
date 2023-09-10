1fd19efe82f034558eb7a6e14962ac05ae4c10ef
Romain <127017159+AI-Romain@users.noreply.github.com>
2023-09-10 19:20:24 +0200

    Delete scripts directory

--- scripts/commands-builder/command.js
+++
@@ -1,7 +0,0 @@
-import CommandsEventHandler from './commands.event.handler';
-export default class Command {
-    constructor() {
-        CommandsEventHandler.addCommand(this);
-    }
-    onExecute(sender, args) { }
-}
